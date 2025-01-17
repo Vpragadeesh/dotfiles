544c444e85eacdd4552a5720c84ace133235eed4
Vpragadeesh <Vpragadeesh@github.com>
2025-01-17 08:25:54 +0530

    6

--- archinstall.sh
+++ archinstall.sh
@@ -32,49 +32,45 @@ mkfs.ext4 "${ROOT}"
 # mount target
 mount "${ROOT}" /mnt
 ROOT_UUID=$(blkid -s UUID -o value "$ROOT")
-mount --mkdir "$EFI" /mnt/boot/efi
+mount --mkdir "$EFI" /mnt/boot
 
 
 echo "--------------------------------------"
 echo "-- INSTALLING Base Arch Linux --"
 echo "--------------------------------------"
-pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware networkmanager vim intel-ucode bluez bluez-utils git btop nvtop  --noconfirm --needed
+pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware networkmanager vim intel-ucode bluez bluez-utils git btop nvtop npm --noconfirm --needed
 
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 # Create a file
-filename="/mnt/root/userinstall.sh"
+filename="/mnt/root/install.sh"
 touch $filename
-  echo "useradd -m -g users -G wheel,storage,power,audio,video -s /usr/bin/fish $USER
-passwd $NAME
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-sed -i 's/^%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+  echo "useradd -m -g users -G wheel,storage,power,audio,video -s /usr/bin/fish $USER" >> install.sh
+echo "passwd $NAME" >>install.sh
+echo "sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers" >> install.sh
 
 echo "-------------------------------------------------"
 echo "Setup Language to US and set locale"
 echo "-------------------------------------------------"
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen" >> install.sh
+echo "locale-gen" >> install.sh
+echo 'echo "LANG=en_US.UTF-8" >> /etc/locale.conf' >> install.sh
+echo "ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime" >> install.sh
+echo "hwclock --systohc" >> install.sh
 
-ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
-hwclock --systohc
-
-echo "HOST" > /etc/hostname
+echo 'echo "HOST" > /etc/hostname' >> install.sh
+echo '
 cat <<EOF > /etc/hosts
 127.0.0.1	localhost
 ::1			localhost
 127.0.1.1	{$HOST}.localdomain	{$HOST}
-echo "-------------------------------------------------"
-echo "NetworkManager and bluetooth"
-echo "-------------------------------------------------"
-systemctl enable NetworkManager bluetooth
-pacman -S grub efibootmgr --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Linux Boot Manager" --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock
-grub-mkconfig -o /boot/grub/grub.cfg
-git clone https://github.com/Vpragadeesh/dotfiles
-" >> $filename
-
-echo "File has been created userinstall.sh
-arch-chroot /mnt
-echo "you need to run userinstall.sh it will be in /home/$USER/userinstall.sh"
+EOF' >> install.sh
+echo 'echo "-------------------------------------------------"' >> install.sh
+echo 'echo "NetworkManager and bluetooth"' >>install.sh
+echo 'echo "-------------------------------------------------"' >>install.sh
+echo "systemctl enable NetworkManager bluetooth" >> install.sh
+echo "pacman -S grub efibootmgr --noconfirm --needed" >> install.sh
+echo "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Linux Boot Manager" --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock" >> install.sh
+echo "grub-mkconfig -o /boot/grub/grub.cfg" >> install.sh
+echo "git clone https://github.com/Vpragadeesh/dotfiles" >> install.sh
+echo "File has been created install.sh"
--- install.sh
+++
@@ -1,6 +0,0 @@
-setfont ter-132b
-pacstrap -K /mnt base linux linux-firmware sof-firmware btop nvtop fish fastfetch vim neovim
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt 
-umount -lR /mnt
-#sudo reboot
