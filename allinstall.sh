ea10f358615a788ca4760d27f9810dea46349100
Vpragadeesh <Vpragadeesh@github.com>
2025-01-24 17:39:50 +0530

    15

--- allinstall.sh
+++ allinstall.sh
@@ -1,78 +1,87 @@
+
 echo "--------------------------------------"
 echo "-- INSTALLING Base Arch Linux --"
 echo "--------------------------------------"
 echo ""
-read -sp "your cpu is amd or intel" igpu
+
+# CPU Selection
+read -sp "Is your CPU AMD or Intel? " igpu
 echo
-if [ "$igpu" = "intel"]; then
+
+# Install base packages
+if [ "$igpu" = "intel" ]; then
   pacstrap /mnt base base-devel linux linux-firmware sof-firmware networkmanager intel-ucode neovim bluez bluez-utils git --noconfirm --needed
 else 
   pacstrap /mnt base base-devel linux linux-firmware sof-firmware networkmanager amd-ucode neovim bluez bluez-utils git --noconfirm --needed
 fi
 
-# fstab
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "getting user info"
+# Prompt for user details
 echo "Please provide details for the new user:"
 read -p "Enter username: " username
 read -p "Enter full name: " fullname
 read -sp "Enter password: " userpassword
+read -p "Enter hostname: " hostname
 echo
+read -p "Enter the default shell for the user (e.g., /bin/bash, /usr/bin/fish): " usershell
 
+# Write next.sh with dynamic inputs
 cat <<REALEND > /mnt/next.sh
-useradd -m $USER
-usermod -c "${NAME}" $USER
-usermod -aG wheel,storage,power,audio,video $USER
-echo $USER:$PASSWORD | chpasswd
+#!/bin/bash
+
+# Add user with the specified shell
+useradd -m -s "$usershell" "$username"
+usermod -c "$fullname" "$username"
+usermod -aG wheel,storage,power,audio,video "$username"
+echo "$username:$userpassword" | chpasswd
+
+# Sudoers setup
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 sed -i 's/^%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
+# Locale and timezone
 echo "-------------------------------------------------"
 echo "Setup Language to US and set locale"
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 
-echo "asus-f15" > /etc/hostname
+# Hostname and hosts file
+echo "$hostname" > /etc/hostname
 cat <<EOF > /etc/hosts
 127.0.0.1	localhost
 ::1			localhost
-127.0.1.1	asus-f15.localdomain	asus-f15
-127.0.0.1   front1.rms.local
-192.168.0.124 front1.rms
-127.0.0.1   front1.ims.local
-192.168.0.153 front1.ims
-127.0.0.1   front1.dpms.local
-192.168.0.164 front1.dpms
+127.0.1.1	$hostname.localdomain	$hostname
 EOF
 
+# Audio Drivers
 echo "-------------------------------------------------"
-echo "Audio Drivers"
-echo "-------------------------------------------------"
-
-pacman -S mesa-utils nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime pipewire pipewire-alsa pipewire-pulse --noconfirm --needed
-
+# Enable services
 systemctl enable NetworkManager bluetooth
-systemctl --user enable pipewire pipewire-pulse
+echo "network and bluetooth started"
+echo "-------------------------------------------------"
 
+# Bootloader installation
 echo "--------------------------------------"
-echo "-- Bootloader Installation  --"
+echo "-- Bootloader Installation --"
+echo "--------------------------------------"
+echo "This will install the GRUB in /boot"
 echo "--------------------------------------"
 
 pacman -S grub efibootmgr --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Linux Boot Manager" --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Linux Boot Manager" --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock
 grub-mkconfig -o /boot/grub/grub.cfg
-
 
 echo "-------------------------------------------------"
 echo "Install Complete, You can reboot now"
 echo "-------------------------------------------------"
-
 REALEND
 
-arch-chroot /mnt sh next.sh
+# Chroot into the installed system and run the script
+arch-chroot /mnt bash /next.sh
