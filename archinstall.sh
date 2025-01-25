53ff49be54c10077142ab3abaf3c654b13a540eb
Vpragadeesh <143618188+Vpragadeesh@users.noreply.github.com>
2025-01-25 22:16:29 +0530

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -2,7 +2,7 @@ setfont ter-120n
 echo "Please set all thing /mnt and /mnt/boot"
 echo "I use /boot only not /boot/efi"
 
-echo "Install Archlinux-keyring"
+echo "Installing Archlinux-keyring"
 pacman -Sy archlinux-keyring
 
 echo "This install will install only base install "
