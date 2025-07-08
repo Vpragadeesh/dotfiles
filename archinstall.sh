d8dfe606f25528afbcc38a97971a0cc15ebfb020
Vpragadeesh <Vpragadeesh@github.com>
2025-07-08 10:54:46 +0530

    added all funtionality

--- archinstall.sh
+++ archinstall.sh
@@ -1,137 +1,167 @@
-setfont ter-120n
-echo "Please set all thing /mnt and /mnt/boot"
-echo "I use /boot only not /boot/efi"
-
-echo "Installing Archlinux-keyring"
-pacman -Sy archlinux-keyring
-
-echo "This install will install the base system along with your chosen Desktop Environment and additional packages"
-echo "--------------------------------------"
-echo "-- INSTALLING Base Arch Linux with DE --"
-echo "--------------------------------------"
-
-# CPU Selection
-read -p "Is your CPU AMD or Intel? " igpu
-echo
-
-# Prompt for Desktop Environment selection:
-echo "Select your Desktop Environment:"
-echo "Options: gnome, plasma, xfce"
-read -p "Enter your choice: " de_choice
-
-# Determine DE packages based on selection (including display manager):
-de_packages=""
-case "$de_choice" in
-  gnome|Gnome)
-    de_packages="gnome gnome-extra gdm"
-    ;;
-  plasma|Plasma)
-    de_packages="plasma sddm"
-    ;;
-  xfce|Xfce)
-    de_packages="xfce4 xfce4-goodies lightdm lightdm-gtk-greeter"
-    ;;
-  *)
-    echo "No valid Desktop Environment selected. Proceeding without DE."
-    ;;
-esac
-
-# Prompt for additional packages to install:
-read -p "Enter any additional packages to install (separated by spaces, or leave blank to skip): " additional_packages
-
-# Install base packages along with DE and additional packages:
-if [ "$igpu" = "intel" ]; then
-  pacstrap /mnt base base-devel linux linux-firmware sof-firmware networkmanager intel-ucode neovim bluez bluez-utils git fish bash $de_packages $additional_packages --noconfirm --needed
-else 
-  pacstrap /mnt base base-devel linux linux-firmware sof-firmware networkmanager amd-ucode neovim bluez bluez-utils git fish bash $de_packages $additional_packages --noconfirm --needed
-fi
-
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Prompt for user details
-echo "Please provide details for the new user:"
-read -p "Enter username: " username
-read -p "Enter full name: " fullname
-read -sp "Enter password: " userpassword
-echo
-read -p "Enter hostname: " hostname
-echo
-read -p "Enter the default shell for the user (e.g., /bin/bash, /usr/bin/fish): " usershell
-
-# Write next.sh with dynamic inputs and service enablement
-cat <<REALEND > /mnt/next.sh
 #!/bin/bash
 
-# Add user with the specified shell
-useradd -m -s "$usershell" "$username"
-usermod -c "$fullname" "$username"
-usermod -aG wheel,storage,power,audio,video "$username"
-echo "$username:$userpassword" | chpasswd
+# Configuration Variables
+TARGET_DISK="/dev/sda" # Change this to your target disk
+HOSTNAME="archlinux"
+USERNAME="user"
+USER_PASSWORD="password"
+ROOT_PASSWORD="rootpassword"
+TIMEZONE="Asia/Kolkata"
+LOCALE="en_US.UTF-8"
+KEYMAP="us"
+SWAP_SIZE="2G"
+BOOT_SIZE="512M"
+FILESYSTEM="ext4" # Options: ext4, btrfs, xfs
+DESKTOP_ENVIRONMENT="gnome" # Options: gnome, plasma, xfce, none
 
-# Sudoers setup
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-sed -i 's/^%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+# Colored Output
+INFO="\e[32m[INFO]\e[0m"
+ERROR="\e[31m[ERROR]\e[0m"
+WARNING="\e[33m[WARNING]\e[0m"
 
-# Locale and timezone
-echo "-------------------------------------------------"
-echo "Setup Language to US and set locale"
-echo "-------------------------------------------------"
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+# Logging
+LOG_FILE="arch_install.log"
+exec > >(tee -i "$LOG_FILE") 2>&1
 
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-hwclock --systohc
+# Error Handling
+set -e
 
-# Hostname and hosts file
-echo "$hostname" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1	localhost
-::1			localhost
-127.0.1.1	$hostname.localdomain	$hostname
+# Functions
+check_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "$ERROR This script must be run as root."
+    exit 1
+  fi
+}
+
+check_internet() {
+  if ! ping -c 1 archlinux.org &>/dev/null; then
+    echo -e "$ERROR No internet connection detected."
+    exit 1
+  fi
+}
+
+update_system_clock() {
+  echo -e "$INFO Updating system clock..."
+  timedatectl set-ntp true
+}
+
+confirm_operation() {
+  read -p "$WARNING Are you sure you want to proceed? (yes/no): " confirmation
+  if [[ "$confirmation" != "yes" ]]; then
+    echo -e "$ERROR Operation aborted."
+    exit 1
+  fi
+}
+
+partition_disk() {
+  echo -e "$INFO Partitioning disk $TARGET_DISK..."
+  confirm_operation
+  parted -s "$TARGET_DISK" mklabel gpt
+  parted -s "$TARGET_DISK" mkpart primary fat32 1MiB "$BOOT_SIZE"
+  parted -s "$TARGET_DISK" set 1 boot on
+  parted -s "$TARGET_DISK" mkpart primary linux-swap "$BOOT_SIZE" "$SWAP_SIZE"
+  parted -s "$TARGET_DISK" mkpart primary "$FILESYSTEM" "$SWAP_SIZE" 100%
+}
+
+format_partitions() {
+  echo -e "$INFO Formatting partitions..."
+  mkfs.fat -F32 "${TARGET_DISK}1"
+  mkswap "${TARGET_DISK}2"
+  swapon "${TARGET_DISK}2"
+  mkfs."$FILESYSTEM" "${TARGET_DISK}3"
+}
+
+mount_partitions() {
+  echo -e "$INFO Mounting partitions..."
+  mount "${TARGET_DISK}3" /mnt
+  mkdir -p /mnt/boot
+  mount "${TARGET_DISK}1" /mnt/boot
+}
+
+install_base_system() {
+  echo -e "$INFO Installing base system..."
+  pacstrap /mnt base base-devel linux linux-firmware networkmanager reflector
+}
+
+generate_fstab() {
+  echo -e "$INFO Generating fstab..."
+  genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+configure_system() {
+  echo -e "$INFO Configuring system in chroot..."
+  arch-chroot /mnt bash <<EOF
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    hwclock --systohc
+    sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+    locale-gen
+    echo "LANG=$LOCALE" > /etc/locale.conf
+    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+    echo "$HOSTNAME" > /etc/hostname
+    cat <<HOSTS > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+HOSTS
+    echo "root:$ROOT_PASSWORD" | chpasswd
+    useradd -m -G wheel -s /bin/bash "$USERNAME"
+    echo "$USERNAME:$USER_PASSWORD" | chpasswd
+    sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+    systemctl enable NetworkManager
 EOF
+}
 
-# Enable core services
-systemctl enable NetworkManager bluetooth
-echo "NetworkManager and bluetooth services enabled"
+install_grub() {
+  echo -e "$INFO Installing GRUB bootloader..."
+  arch-chroot /mnt bash <<EOF
+    pacman -S grub efibootmgr --noconfirm
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+    grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+}
 
-# Bootloader installation
-echo "--------------------------------------"
-echo "-- Bootloader Installation --"
-echo "--------------------------------------"
-echo "This will install GRUB in /boot"
-echo "--------------------------------------"
-pacman -S grub efibootmgr --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Linux Boot Manager" --modules="normal test efi_gop efi_uga search echo linux all_video gfxmenu gfxterm_background gfxterm_menu gfxterm loadenv configfile tpm" --disable-shim-lock
-grub-mkconfig -o /boot/grub/grub.cfg
+install_desktop_environment() {
+  if [[ "$DESKTOP_ENVIRONMENT" != "none" ]]; then
+    echo -e "$INFO Installing Desktop Environment: $DESKTOP_ENVIRONMENT..."
+    arch-chroot /mnt bash <<EOF
+      case "$DESKTOP_ENVIRONMENT" in
+        gnome)
+          pacman -S gnome gnome-extra gdm --noconfirm
+          systemctl enable gdm
+          ;;
+        plasma)
+          pacman -S plasma sddm --noconfirm
+          systemctl enable sddm
+          ;;
+        xfce)
+          pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm
+          systemctl enable lightdm
+          ;;
+      esac
+EOF
+  fi
+}
 
-# Enable the chosen Desktop Environment's display manager:
-case "$de_choice" in
-  gnome|Gnome)
-    systemctl enable gdm
-    echo "Gnome Display Manager (GDM) enabled"
-    ;;
-  plasma|Plasma)
-    systemctl enable sddm
-    echo "Plasma Display Manager (SDDM) enabled"
-    ;;
-  xfce|Xfce)
-    systemctl enable lightdm
-    echo "XFCE Display Manager (LightDM) enabled"
-    ;;
-  *)
-    echo "No Desktop Environment Display Manager to enable."
-    ;;
-esac
+update_mirror_list() {
+  echo -e "$INFO Updating mirror list..."
+  reflector --country India --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+}
 
-echo "-------------------------------------------------"
-echo "Installation Complete, You can reboot now"
-echo "-------------------------------------------------"
-REALEND
+main() {
+  check_root
+  check_internet
+  update_system_clock
+  partition_disk
+  format_partitions
+  mount_partitions
+  install_base_system
+  generate_fstab
+  configure_system
+  install_grub
+  update_mirror_list
+  install_desktop_environment
+  echo -e "$INFO Installation complete. You can reboot now."
+}
 
-# Make next.sh executable
-chmod +x /mnt/next.sh
-
-# Chroot into the installed system and run the script
-arch-chroot /mnt bash /next.sh
+main
