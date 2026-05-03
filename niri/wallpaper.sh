185dd96ac9cdb7c47fc54ce8732ba2a4905b3f76
Vpragadeesh <vpragadeesh.aiml2023@citchennai.net>
2026-05-03 22:33:09 +0530

    upd: niri

--- niri/wallpaper.sh
+++ niri/wallpaper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pkill swaybg || true
-swaybg -i "/home/pragadeesh/wallpaper/wallpapers/Sun-Setting-Horizon.png" -m fill &
+swaybg -i "/home/pragadeesh/wallpaper/Don't negotiate with your goals; instead increase the effort.png" -m fill &
+# swaybg -i "/home/pragadeesh/wallpaper/Consistency_2quotes_upscayl_16x_upscayl-standard-4x.png" -m fill &
